cf716a339161bfd367bf4dc65af4ec89b44e2eb7
adonath <axel.donath@mpi-hd.mpg.de>
2018-09-04 18:59:00 +0200

    Split background reference into iso and gal contributions

--- 3fhl/gc/fermi-3fhl-gc-data-prepare.sh
+++ 3fhl/gc/fermi-3fhl-gc-data-prepare.sh
@@ -1,14 +1,14 @@
-# This is as script to prepare Fermi 3FHL gc all sky data
+# This is as script to prepare Fermi 3FHL gc  data
 
 # Basic analysis Parameters
 EMIN=10000
-EMAX=30000
+EMAX=500000
 ZMAX=105
 EVENT_CLASS=128
 EVENT_TYPE=3
 IRF=P8R2_SOURCE_V6 # Corresponding to event type 128
 EBINALG=LOG
-NEBINS=1
+NEBINS=11
 
 # Time cuts taken from 3FHL paper
 TMIN=239557417.
@@ -25,23 +25,32 @@ EVENTS_SELECTED=fermi-3fhl-gc-events-selected.fits
 EXPOSURE=fermi-3fhl-gc-exposure.fits
 COUNTS=fermi-3fhl-gc-counts.fits
 PSF=fermi-3fhl-psf-gc.fits
-BACKGROUND=fermi-3fhl-gc-background-npred.fits
-MODEL=background-model.xml
+BACKGROUND_ALL=fermi-3fhl-gc-background-all.fits
+BACKGROUND_ISO=fermi-3fhl-gc-background-iso.fits
+BACKGROUND_GAL=fermi-3fhl-gc-background-gal.fits
+BKG_MODEL_ALL=background-model-all.xml
+BKG_MODEL_GAL=background-model-gal.xml
+BGK_MODEL_ISO=background-model-iso.xml
 
 # # Counts cube
-# gtbin evfile=$EVENTS outfile=$COUNTS algorithm=CCUBE SCFILE=$SPACECRAFT \
-#       coordsys=GAL binsz=0.05 nxpix=200 nypix=100 xref=0 yref=0 proj=CAR ebinalg=LOG \
-#       emin=$EMIN emax=$EMAX enumbins=$NEBINS axisrot=0
+gtbin evfile=$EVENTS outfile=$COUNTS algorithm=CCUBE SCFILE=$SPACECRAFT \
+      coordsys=GAL binsz=0.05 nxpix=200 nypix=100 xref=0 yref=0 proj=CAR ebinalg=LOG \
+      emin=$EMIN emax=$EMAX enumbins=$NEBINS axisrot=0
 
 # Compute exposure cube
 gtexpcube2 infile=$LIVETIME outfile=$EXPOSURE irf=$IRF coordsys=GAL binsz=0.05 \
       nxpix=200 nypix=100 xref=0 yref=0 proj=CAR ebinalg=$EBINALG cmap=none \
       emin=$EMIN emax=$EMAX enumbins=$NEBINS axisrot=0 bincalc=EDGE
 
-# # Compute psf cube
-# gtpsf expcube=$LIVETIME outfile=$PSF irfs=$IRF ra=266.42 dec=-29.01 \
-# emin=$EMIN emax=$EMAX nenergies=$NEBINS thetamax=10 ntheta=300
+# Compute psf cube
+gtpsf expcube=$LIVETIME outfile=$PSF irfs=$IRF ra=266.42 dec=-29.01 \
+emin=$EMIN emax=$EMAX nenergies=$NEBINS thetamax=10 ntheta=300
 
-# gtmodel srcmaps=$COUNTS bexpmap=$EXPOSURE expcube=$LIVETIME srcmdl=$MODEL \
-#        irfs=$IRF outfile=$BACKGROUND evtype=$EVENT_TYPE outtype=ccube
+gtmodel srcmaps=$COUNTS bexpmap=$EXPOSURE expcube=$LIVETIME srcmdl=$BKG_MODEL_ALL \
+       irfs=$IRF outfile=$BACKGROUND_ALL evtype=$EVENT_TYPE outtype=ccube
 
+gtmodel srcmaps=$COUNTS bexpmap=$EXPOSURE expcube=$LIVETIME srcmdl=$BKG_MODEL_GAL \
+       irfs=$IRF outfile=$BACKGROUND_GAL evtype=$EVENT_TYPE outtype=ccube
+
+gtmodel srcmaps=$COUNTS bexpmap=$EXPOSURE expcube=$LIVETIME srcmdl=$BGK_MODEL_ISO \
+       irfs=$IRF outfile=$BACKGROUND_ISO evtype=$EVENT_TYPE outtype=ccube
